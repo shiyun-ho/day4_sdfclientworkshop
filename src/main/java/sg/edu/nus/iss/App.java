8c5c7251228f5e2465a2df3f2b800724edeea9c4
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-17 15:14:02 +0800

    client side

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,15 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -10,8 +20,32 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
+     * @throws UnknownHostException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("localhost", 7000);
+
+        try(OutputStream os = socket.getOutputStream()) 
+        {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+            
+            //reading data
+            Console con = System.console();
+            String readInput = "";;
+
+            while (!readInput.equalsIgnoreCase("close")){
+                readInput = con.readLine("Enter a command: "); 
+                dos.writeUTF(readInput);
+                dos.flush();
+            }
+
+            
+
+            //handle end of file exception
+        } catch (EOFException ex) {
+            socket.close();
+        }
     }
 }
