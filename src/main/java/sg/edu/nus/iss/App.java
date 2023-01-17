d7850fb73b84c9b3ca2f55d175d53604a861af3e
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-17 15:47:23 +0800

    final version

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -3,12 +3,15 @@ package sg.edu.nus.iss;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.Console;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.Buffer;
 
 /**
  * Hello world!
@@ -26,6 +29,7 @@ public final class App {
     public static void main(String[] args) throws UnknownHostException, IOException {
         Socket socket = new Socket("localhost", 7000);
 
+        //sends msg
         try(OutputStream os = socket.getOutputStream()) 
         {
             BufferedOutputStream bos = new BufferedOutputStream(os);
@@ -33,12 +37,26 @@ public final class App {
             
             //reading data
             Console con = System.console();
-            String readInput = "";;
+            String readInput = "", receivedMessaged = "";
 
-            while (!readInput.equalsIgnoreCase("close")){
+            try(InputStream is = socket.getInputStream()){
+                BufferedInputStream bis = new BufferedInputStream(is);
+                DataInputStream dis = new DataInputStream(bis);
+               
+                while (!readInput.equalsIgnoreCase("close")){
                 readInput = con.readLine("Enter a command: "); 
                 dos.writeUTF(readInput);
                 dos.flush();
+
+                receivedMessaged = dis.readUTF();
+                System.out.println(receivedMessaged);
+                }
+
+                dis.close();
+                bis.close();
+
+            } catch (EOFException ex){
+                socket.close();
             }
 
             
